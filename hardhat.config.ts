eb5d4a19df6ec3a1da1f9b26ed86faefb56d0c9a
theBlastOS <160002055+theBlastOS@users.noreply.github.com>
2025-08-24 20:19:07 +0800

    deploy sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,17 +6,22 @@ import "@typechain/hardhat";
 import "hardhat-deploy";
 import "hardhat-gas-reporter";
 import type { HardhatUserConfig } from "hardhat/config";
-import { vars } from "hardhat/config";
 import "solidity-coverage";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 import "./tasks/accounts";
 import "./tasks/FHECounter";
 import "./tasks/IPFSEncryptedStorage";
 
-// Run 'npx hardhat vars setup' to see the list of variables that need to be set
+// 使用 .env 文件配置
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY || "";
+const INFURA_API_KEY: string = process.env.INFURA_API_KEY || "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
+const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY || "";
 
-const MNEMONIC: string = vars.get("MNEMONIC", "test test test test test test test test test test test junk");
-const INFURA_API_KEY: string = vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+// 测试用的助记词，仅用于本地开发
+const MNEMONIC: string = "test test test test test test test test test test test junk";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -25,7 +30,7 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      sepolia: vars.get("ETHERSCAN_API_KEY", ""),
+      sepolia: ETHERSCAN_API_KEY,
     },
   },
   gasReporter: {
@@ -50,13 +55,15 @@ const config: HardhatUserConfig = {
       url: "http://localhost:8545",
     },
     sepolia: {
-      accounts: {
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : {
         mnemonic: MNEMONIC,
         path: "m/44'/60'/0'/0/",
         count: 10,
       },
       chainId: 11155111,
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      url: INFURA_API_KEY !== "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz" 
+        ? `https://sepolia.infura.io/v3/${INFURA_API_KEY}`
+        : "https://ethereum-sepolia-rpc.publicnode.com",
     },
   },
   paths: {
